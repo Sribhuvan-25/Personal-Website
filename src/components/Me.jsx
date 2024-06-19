c990f214bd004671aae160ba1156fdd137c27162
Sribhuvan-25 <sribhuvan.reddy@gmail.com>
2024-06-19 15:21:07 -0400

    Added animation using framer motion

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,4 +1,15 @@
 import profile from "../assets/profile.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: {x: -100, opacity: 0},
+  visible: {
+    x:0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay},
+  },
+
+})
 
 const Me = () => {
   return (
@@ -6,18 +17,41 @@ const Me = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1 
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
+            className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Sribhuvan Yellu
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
-            bg-clip-text text-4xl tracking-tight text-transparent"> Developer</span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas facilis eum similique ad dolore. 
-              Nam, distinctio quia obcaecati explicabo perspiciatis accusamus, omnis suscipit dolorem ut atque libero nemo earum fugiat.</p>
+            </motion.h1>
+            <motion.span 
+            variants={container(0.2)}
+            initial="hidden"
+            animate="visible"
+            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
+            bg-clip-text text-4xl tracking-tight text-transparent"> 
+              Developer
+            </motion.span>
+            <motion.p 
+            variants={container(0.4)}
+            initial="hidden"
+            animate="visible"
+            className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas facilis eum similique ad dolore. 
+              Nam, distinctio quia obcaecati explicabo perspiciatis accusamus, omnis suscipit dolorem ut atque libero nemo earum fugiat.
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profile} alt="Sirbhuvan" />
+            <motion.img
+            className="rounded-2xl"
+            initial={{x: 100, opacity: 0}}
+            animate={{x: 0, opacity: 1}}
+            transition={{duration: 1, delay: 0.8}}
+            src={profile} 
+            alt="Sirbhuvan"
+            />
           </div>
         </div>
       </div>
