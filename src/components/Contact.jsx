f3b4b5dd5113c0f6a79e6a339432ead045a53f7e
Sribhuvan <77555733+Sribhuvan-25@users.noreply.github.com>
2025-05-24 23:36:14 -0400

    Layout changes (#5)
    * UI changes
    
    * Clean up
    
    * Clean up
    
    * Clean up
    
    * Fix: Add missing image assets and move old images to Old folder
    
    * Typo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import { CONTACT } from "../constants"
 
 const Contact = () => {
   return (
-    <div className="border-b border-neutral-900 pb-20">
+    <div className="pb-20">
       <h2 className="my-10 text-center text-4xl">Get in Touch </h2>
       <div className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.address}</p>
