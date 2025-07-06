b78ef703ba8bbdccd3e92f28a6759f9125b244fb
Backspace Agent <backspace-agent@example.com>
2025-07-06 06:17:29 +0000

    feat: Add a simple header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="bg-gray-800 p-4">
+            <ul className="flex space-x-4">
+                <li><a href="#home" className="text-white">Home</a></li>
+                <li><a href="#about" className="text-white">About</a></li>
+                <li><a href="#projects" className="text-white">Projects</a></li>
+                <li><a href="#contact" className="text-white">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
